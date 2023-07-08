118c45582a2eeacbe00b32468d209633bfaf61a4
Sh4d0wless <61610498+Sh4d0wless@users.noreply.github.com>
2023-07-09 00:00:58 +0200

    `config.php

--- `config.php
+++ `config.php
@@ -1,11 +1,14 @@
 <?php
 /* 
-    AMX MONITORING v.1.1.6
-	By Virtus-pro
+     AMX MONITORING v.1.1.6
+	Разработчик: Virtus-pro
 
-	www.amxservers.ru 
+	Сайт: www.amxservers.ru 
+	
+	******Спасибо. Надеемся Вам понравится наш продукт.********
 */
 
+// Настройки базы данных
 $db_host = "127.0.0.1";
 $db_user = "DB_UserName";
 $db_pass = "DB_UserPass";
