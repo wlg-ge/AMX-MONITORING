bce4646bb962bee88662f6e6490d4c94f5fbeb9a
Sh4d0wless <61610498+Sh4d0wless@users.noreply.github.com>
2023-07-08 22:52:02 +0200

    Add files via upload

---
+++ `config.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+    AMX MONITORING v.1.1.6
+	By Virtus-pro
+
+	www.amxservers.ru 
+*/
+
+$db_host = "127.0.0.1";
+$db_user = "DB_UserName";
+$db_pass = "DB_UserPass";
+$db_name = "DB_Name";
+$db_prefix = "amx_";
+define("DB_PREFIX", "amx_");
+?>
